4d9a3e50a4a95f101060d395658b4e963617f417
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-14 15:18:50 -0500

    update bootstrap

--- 2024/bootstrap-day.sh
+++ 2024/bootstrap-day.sh
@@ -25,29 +25,51 @@ cat > main.go <<EOL
 package main
 
 import (
-    "bufio"
-    "fmt"
-    "os"
+	"bufio"
+	"fmt"
+	"os"
 )
 
-func main() {
-    file, err := os.Open("input.txt")
-    if err != nil {
-        fmt.Println("Error reading input file:", err)
-        return
-    }
-    defer file.Close()
-
-    scanner := bufio.NewScanner(file)
-    for scanner.Scan() {
-        line := scanner.Text()
-        fmt.Println(line)
-    }
-
-    if err := scanner.Err(); err != nil {
-        fmt.Println("Error scanning input:", err)
-    }
+func solve1(lines []string) (string, error) {
+	return "TODO", nil
 }
+
+func solve2(lines []string) (string, error) {
+	return "TODO", nil
+}
+
+func readFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func main() {
+	lines, err := readFile("input.txt")
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input.txt", err)
+		os.Exit(1)
+	}
+	answer, _ := solve1(lines)
+	fmt.Printf("Puzzle 1 Answer: %s\n", answer)
+
+	answer2, _ := solve2(lines)
+	fmt.Printf("Puzzle 2 Answer: %s\n", answer2)
+}
+
 EOL
 
 # Create input files
