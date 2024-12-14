c5932743c757236529a7c559a04e7602a46aeadd
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-14 14:50:55 -0500

    refactor so we can do both puzzles

--- 2024/day-01/main.go
+++ 2024/day-01/main.go
@@ -9,33 +9,25 @@ import (
 	"strings"
 )
 
-func readListsFromFile(fileName string) ([]int, []int, error) {
-	file, err := os.Open(fileName)
-	if err != nil {
-		return nil, nil, err
-	}
-	defer file.Close()
-
+func solve1(lines []string) (string, error) {
 	var list_one []int
 	var list_two []int
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		line := scanner.Text()
+	for _, line := range lines {
 		words := strings.Fields(line)
 
 		if len(words) != 2 {
-			return nil, nil, fmt.Errorf("invalid line format: '%s'", line)
+			return "", fmt.Errorf("invalid line format: '%s'", line)
 		}
 
 		int1, err := strconv.Atoi(words[0])
 		if err != nil {
-			return nil, nil, fmt.Errorf("error converting '%s' to int: '%v'", words[0], err)
+			return "", fmt.Errorf("error converting '%s' to int: '%v'", words[0], err)
 		}
 
 		int2, err := strconv.Atoi(words[1])
 		if err != nil {
-			return nil, nil, fmt.Errorf("error converting '%s' to int: '%v'", words[1], err)
+			return "", fmt.Errorf("error converting '%s' to int: '%v'", words[1], err)
 		}
 
 		list_one = append(list_one, int1)
@@ -44,28 +36,46 @@ func readListsFromFile(fileName string) ([]int, []int, error) {
 
 	slices.Sort(list_one)
 	slices.Sort(list_two)
-	return list_one, list_two, nil
-}
-
-func main() {
-	list_one, list_two, err := readListsFromFile("input.txt")
-	if err != nil {
-		fmt.Printf("Error parsing file: '%v'\n", err)
-		os.Exit(1)
-	}
 
 	var total_distance int
 	for i := 0; i < len(list_one); i++ {
 		n1 := list_one[i]
 		n2 := list_two[i]
 		d := n1 - n2
-		fmt.Println(n1, n2, d)
 		if d < 0 {
 			d = -d
 		}
-		fmt.Println(d)
 		total_distance += d
-		fmt.Println(total_distance)
 	}
-	fmt.Printf("The total distance is: %d\n", total_distance)
+
+	return fmt.Sprintf("%d", total_distance), nil
+}
+
+func readFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func main() {
+	lines, err := readFile("input.txt")
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input.txt", err)
+		os.Exit(1)
+	}
+	answer, _ := solve1(lines)
+	fmt.Printf("Puzzle 1 Answer: %s\n", answer)
 }
