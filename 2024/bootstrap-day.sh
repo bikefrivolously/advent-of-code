4fca41a23c694151a82e7f49d15672d74d57ded2
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-14 21:25:44 -0500

    update bootstrap to create tests

--- 2024/bootstrap-day.sh
+++ 2024/bootstrap-day.sh
@@ -72,6 +72,43 @@ func main() {
 
 EOL
 
+cat > main_test.go <<EOL
+package main
+
+import (
+	"testing"
+)
+
+func TestSolve1(t *testing.T) {
+	lines, err := readFile("test_input.txt")
+	if err != nil {
+		t.Fatalf("Error reading input file '%s': %v\n", "test_input.txt", err)
+	}
+
+	expected := "2"
+	result, _ := solve1(lines)
+
+	if result != expected {
+		t.Errorf("solve1() = %s; want %s", result, expected)
+	}
+}
+
+func TestSolve2(t *testing.T) {
+	lines, err := readFile("test_input.txt")
+	if err != nil {
+		t.Fatalf("Error reading input file '%s': %v\n", "test_input.txt", err)
+	}
+
+	expected := "4"
+	result, _ := solve2(lines)
+
+	if result != expected {
+		t.Errorf("solve2() = %s; want %s", result, expected)
+	}
+}
+
+EOL
+
 # Create input files
 touch input.txt test_input.txt
 
