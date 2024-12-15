26228a1804c74c86cee430ed85dcb7b190ade363
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-15 11:16:44 -0500

    day 3 part 1 and 2

--- 2024/day-03/main.go
+++ 2024/day-03/main.go
@@ -4,14 +4,43 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 )
 
 func solve1(lines []string) (string, error) {
-	return "TODO", nil
+	var answer int
+	re, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)`)
+	for _, line := range lines {
+		matches := re.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			x, _ := strconv.Atoi(match[1])
+			y, _ := strconv.Atoi(match[2])
+			answer += x * y
+		}
+	}
+	return fmt.Sprintf("%d", answer), nil
 }
 
 func solve2(lines []string) (string, error) {
-	return "TODO", nil
+	var answer int
+	re, _ := regexp.Compile(`mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)`)
+	enabled := true
+	for _, line := range lines {
+		matches := re.FindAllStringSubmatch(line, -1)
+		for _, match := range matches {
+			if match[0] == "do()" {
+				enabled = true
+			} else if match[0] == "don't()" {
+				enabled = false
+			} else if enabled {
+				x, _ := strconv.Atoi(match[1])
+				y, _ := strconv.Atoi(match[2])
+				answer += x * y
+			}
+		}
+	}
+	return fmt.Sprintf("%d", answer), nil
 }
 
 func readFile(filePath string) ([]string, error) {
@@ -45,4 +74,3 @@ func main() {
 	answer2, _ := solve2(lines)
 	fmt.Printf("Puzzle 2 Answer: %s\n", answer2)
 }
-
--- 2024/day-03/main_test.go
+++ 2024/day-03/main_test.go
@@ -10,7 +10,7 @@ func TestSolve1(t *testing.T) {
 		t.Fatalf("Error reading input file '%s': %v\n", "test_input.txt", err)
 	}
 
-	expected := "2"
+	expected := "161"
 	result, _ := solve1(lines)
 
 	if result != expected {
@@ -19,16 +19,15 @@ func TestSolve1(t *testing.T) {
 }
 
 func TestSolve2(t *testing.T) {
-	lines, err := readFile("test_input.txt")
+	lines, err := readFile("test_input2.txt")
 	if err != nil {
-		t.Fatalf("Error reading input file '%s': %v\n", "test_input.txt", err)
+		t.Fatalf("Error reading input file '%s': %v\n", "test_input2.txt", err)
 	}
 
-	expected := "4"
+	expected := "48"
 	result, _ := solve2(lines)
 
 	if result != expected {
 		t.Errorf("solve2() = %s; want %s", result, expected)
 	}
 }
-
