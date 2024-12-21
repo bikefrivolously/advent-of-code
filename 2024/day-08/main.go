df460888539d0376329de0667b1a9e2c33b9f4cc
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-21 18:21:35 -0500

    day 8 init

---
+++ 2024/day-08/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func solve1(lines []string) (string, error) {
+	var answer int
+
+	answer = -1
+	return fmt.Sprintf("%d", answer), nil
+}
+
+func solve2(lines []string) (string, error) {
+	var answer int
+
+	answer = -1
+	return fmt.Sprintf("%d", answer), nil
+}
+
+func readFile(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func main() {
+	lines, err := readFile("input.txt")
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input.txt", err)
+		os.Exit(1)
+	}
+	answer, _ := solve1(lines)
+	fmt.Printf("Puzzle 1 Answer: %s\n", answer)
+
+	answer2, _ := solve2(lines)
+	fmt.Printf("Puzzle 2 Answer: %s\n", answer2)
+}
+
