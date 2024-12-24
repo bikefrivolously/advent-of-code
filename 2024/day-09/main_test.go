f8bd63aee919609977e7bd7d37a124f527b96dc0
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-24 07:42:48 -0500

    benchmarks for hard and evil

--- 2024/day-09/main_test.go
+++ 2024/day-09/main_test.go
@@ -56,3 +56,27 @@ func BenchmarkSolve2(b *testing.B) {
 		solve2(lines)
 	}
 }
+
+func BenchmarkSolve2Hard(b *testing.B) {
+	lines, err := readFile("input_hard1.txt", nil)
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input_hard1.txt", err)
+		panic("crash")
+	}
+	b.ResetTimer()
+	for range b.N {
+		solve2(lines)
+	}
+}
+
+func BenchmarkSolve2Evil(b *testing.B) {
+	lines, err := readFile("input_evil1.txt", &ReadFileOptions{BufferSize: 200 * 1024})
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input_evil1.txt", err)
+		panic("crash")
+	}
+	b.ResetTimer()
+	for range b.N {
+		solve2(lines)
+	}
+}
