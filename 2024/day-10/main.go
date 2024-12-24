4e42d3ede13abc88744cf81ed07714843458086e
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-24 15:05:48 -0500

    day 10 init

---
+++ 2024/day-10/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"time"
+)
+
+func solve1(lines []string) (string, error) {
+	var answer int
+
+	answer = -1
+	return fmt.Sprintf("%d", answer), nil
+}
+
+func solve2(lines []string) (string, error) {
+	var answer int
+
+	answer = -1
+	return fmt.Sprintf("%d", answer), nil
+}
+
+type ReadFileOptions struct {
+	BufferSize int
+}
+
+func readFile(filePath string, options *ReadFileOptions) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	if options != nil && options.BufferSize > 0 {
+		buf := make([]byte, options.BufferSize)
+		scanner.Buffer(buf, options.BufferSize)
+	}
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	err = scanner.Err()
+	if err != nil {
+		return nil, err
+	}
+	return lines, nil
+}
+
+func main() {
+	lines, err := readFile("input.txt", nil)
+	if err != nil {
+		fmt.Printf("Error reading input file '%s': %v\n", "input.txt", err)
+		os.Exit(1)
+	}
+	start := time.Now()
+	answer, _ := solve1(lines)
+	duration := time.Since(start)
+	fmt.Printf("Puzzle 1 Answer: %s (runtime: %v)\n", answer, duration)
+
+	start = time.Now()
+	answer2, _ := solve2(lines)
+	duration = time.Since(start)
+	fmt.Printf("Puzzle 2 Answer: %s (runtime: %v)\n", answer2, duration)
+}
+
