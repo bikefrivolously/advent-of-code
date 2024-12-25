6b90a396b74b3949651200d5b2d9e7b0f49567a5
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-24 22:13:18 -0500

    day 11 part 1

--- 2024/day-11/main.go
+++ 2024/day-11/main.go
@@ -2,23 +2,98 @@ package main
 
 import (
 	"bufio"
+	"container/list"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
+
+func numDigits(i int) int {
+	if i == 0 {
+		return 1
+	}
+	digits := 0
+	for i > 0 {
+		i /= 10
+		digits++
+	}
+	return digits
+}
+
+func intPower(base, exp int) int {
+	if exp == 0 {
+		return 1
+	}
+
+	result := 1
+	for exp > 0 {
+		result *= base
+		exp--
+	}
+	return result
+}
+
+func countAfterBlinks(i int, blinks int) int {
+	// fmt.Printf("countAfterBlinks: i=%d, blinks=%d\n", i, blinks)
+	if blinks == 0 {
+		return 1
+	}
+	if i == 0 {
+		return countAfterBlinks(1, blinks-1)
+	} else if digits := numDigits(i); digits%2 == 0 {
+		// split in two
+		l := i / intPower(10, digits/2)
+		r := i - (l * intPower(10, digits/2))
+		// fmt.Printf("i: %d, l: %d, r: %d\n", i, l, r)
+
+		return countAfterBlinks(l, blinks-1) + countAfterBlinks(r, blinks-1)
+	} else {
+		return countAfterBlinks(i*2024, blinks-1)
+	}
+}
 
 func solve1(lines []string) (string, error) {
 	var answer int
 
-	answer = -1
+	queue := parseLines(lines)
+	for e := queue.Front(); e != nil; e = e.Next() {
+		answer += countAfterBlinks(e.Value.(int), 25)
+	}
+
 	return fmt.Sprintf("%d", answer), nil
 }
 
 func solve2(lines []string) (string, error) {
 	var answer int
 
-	answer = -1
+	queue := parseLines(lines)
+	for e := queue.Front(); e != nil; e = e.Next() {
+		answer += countAfterBlinks(e.Value.(int), 75)
+	}
+
 	return fmt.Sprintf("%d", answer), nil
+}
+
+func mustAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic("failed to convert string to int")
+	}
+	return i
+}
+
+func parseLines(lines []string) *list.List {
+	l := list.New()
+	for _, line := range lines {
+		nums := strings.Fields(line)
+		for _, num := range nums {
+			i := mustAtoi(num)
+			l.PushBack(i)
+		}
+	}
+	return l
 }
 
 type ReadFileOptions struct {
@@ -66,4 +141,3 @@ func main() {
 	duration = time.Since(start)
 	fmt.Printf("Puzzle 2 Answer: %s (runtime: %v)\n", answer2, duration)
 }
-
--- 2024/day-11/main_test.go
+++ 2024/day-11/main_test.go
@@ -11,7 +11,7 @@ func TestSolve1(t *testing.T) {
 		t.Fatalf("Error reading input file '%s': %v\n", "test_input.txt", err)
 	}
 
-	expected := "-1"
+	expected := "55312"
 	result, _ := solve1(lines)
 
 	if result != expected {
@@ -56,4 +56,3 @@ func BenchmarkSolve2(b *testing.B) {
 		solve2(lines)
 	}
 }
-
