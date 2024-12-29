a34b30ea0705003cbbb23b31b1864c61e3875418
Dan Martins <19761930+bikefrivolously@users.noreply.github.com>
2024-12-29 16:08:53 -0500

    day 13 part 1

--- 2024/day-13/main.go
+++ 2024/day-13/main.go
@@ -4,13 +4,57 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 	"time"
 )
+
+type EquationPair struct {
+	one LinearEquation
+	two LinearEquation
+}
+
+type LinearEquation struct {
+	aCoefficent int
+	bCoefficent int
+	constant    int
+}
+
+func mustAtoi(s string) int {
+	i, err := strconv.Atoi(s)
+	if err != nil {
+		panic("failed to convert string to int")
+	}
+	return i
+}
+
+func solveLinearSystem(eq1 LinearEquation, eq2 LinearEquation, limit int) (int, int, error) {
+	for b := limit; b >= 0; b-- {
+		for a := 0; a <= limit; a++ {
+			// ans1 := a*eq1.aCoefficent + b*eq1.bCoefficent
+			// ans2 := b*eq2.aCoefficent + b*eq2.bCoefficent
+
+			if a*eq1.aCoefficent+b*eq1.bCoefficent == eq1.constant && a*eq2.aCoefficent+b*eq2.bCoefficent == eq2.constant {
+				fmt.Printf("Solved: %d, %d\n", a, b)
+				return a, b, nil
+			}
+		}
+	}
+	return 0, 0, fmt.Errorf("no solution for %v %v", eq1, eq2)
+}
 
 func solve1(lines []string) (string, error) {
 	var answer int
 
-	answer = -1
+	equations := parseLines(lines)
+	for _, system := range equations {
+		a, b, err := solveLinearSystem(system.one, system.two, 100)
+		if err != nil {
+			continue
+		}
+		answer += 3*a + 1*b
+	}
+
 	return fmt.Sprintf("%d", answer), nil
 }
 
@@ -23,6 +67,31 @@ func solve2(lines []string) (string, error) {
 
 type ReadFileOptions struct {
 	BufferSize int
+}
+
+func parseLines(lines []string) []EquationPair {
+	reA := regexp.MustCompile(`Button A: X\+(\d+), Y\+(\d+)`)
+	reB := regexp.MustCompile(`Button B: X\+(\d+), Y\+(\d+)`)
+	rePrize := regexp.MustCompile(`Prize: X=(\d+), Y=(\d+)`)
+
+	var equations []EquationPair
+	var currentPair *EquationPair
+
+	for _, line := range lines {
+		if match := reA.FindStringSubmatch(line); match != nil {
+			currentPair = &EquationPair{}
+			(*currentPair).one.aCoefficent = mustAtoi(match[1])
+			(*currentPair).two.aCoefficent = mustAtoi(match[2])
+		} else if match := reB.FindStringSubmatch(line); match != nil {
+			(*currentPair).one.bCoefficent = mustAtoi(match[1])
+			(*currentPair).two.bCoefficent = mustAtoi(match[2])
+		} else if match := rePrize.FindStringSubmatch(line); match != nil {
+			(*currentPair).one.constant = mustAtoi(match[1])
+			(*currentPair).two.constant = mustAtoi(match[2])
+			equations = append(equations, *currentPair)
+		}
+	}
+	return equations
 }
 
 func readFile(filePath string, options *ReadFileOptions) ([]string, error) {
@@ -66,4 +135,3 @@ func main() {
 	duration = time.Since(start)
 	fmt.Printf("Puzzle 2 Answer: %s (runtime: %v)\n", answer2, duration)
 }
-
